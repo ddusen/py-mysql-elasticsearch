16eeb3ed94200a1b8874473bfb23c9ddcf91d39f
Sdu <sdu@shendu.info>
2018-05-07 15:01:59 +0800

    add reading config.ini method

--- core/sync.py
+++ core/sync.py
@@ -1,26 +1,48 @@
+from configparser import ConfigParser
 from elasticsearch import Elasticsearch
 
 
-def main():
+# 读取 config.ini 配置项
+def config():
+    cfg = ConfigParser()
+    cfg.read('core/config.ini')
+
+    return {'mysql':
+                {
+                    'host': cfg.get('mysql', 'host'),
+                    'port': cfg.get('mysql', 'port'),
+                    'user': cfg.get('mysql', 'user'),
+                    'passwd': cfg.get('mysql', 'passwd'),
+                    'charset': cfg.get('mysql', 'charset'),
+                },
+            'elasticsearch':
+                {
+                    'host': cfg.get('elasticsearch', 'host'),
+                    'port': cfg.get('elasticsearch', 'port'),
+                    'index': cfg.get('elasticsearch', 'index'),
+                    'type': cfg.get('elasticsearch', 'type'),
+                },
+            }
+
+
+def es():
     esclient = Elasticsearch(['localhost:9200'])
     response = esclient.search(
-        index='social-*',
+        index='observer',
         body={
             "query": {
                 "match": {
-                    "message": "myProduct"
-                }
-            },
-            "aggs": {
-                "top_10_states": {
-                    "terms": {
-                        "field": "state",
-                        "size": 10
-                    }
+                    "title": "儿童"
                 }
             }
         }
     )
+    print(response)
+
+
+def main():
+    
+
 
 if __name__ == '__main__':
-    main()
+    print(main())
